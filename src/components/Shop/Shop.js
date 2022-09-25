a6d0641585a9cd72eb6358698034f145503e4cc6
sufian <sufian4se@gmail.com>
2022-09-25 23:37:07 +0600

    display local storage cart to the ui

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,18 +9,30 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect( () => {
+        console.log('product load before fetch');
         fetch('products.json')
         .then(res => res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data);
+            console.log('products loaded')
+        })
     }, []);
 
     useEffect( () => {
+        console.log('local storage first line');
         const storedCart = getStoredCart();
+        const savedCart = [];
         for(const id in storedCart) {
             const addedProduct = products.find(product => product.id === id);
-            console.log(addedProduct)
+            if(addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
         }
-    }, [])
+        setCart(savedCart);
+        console.log('local storage finished')
+    }, [products])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
