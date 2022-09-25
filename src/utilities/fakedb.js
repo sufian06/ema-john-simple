cafce15e6c37da4ca8b463f142de6bf348602d34
sufian <sufian4se@gmail.com>
2022-09-25 23:15:57 +0600

    load cart from local storage, find product

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -19,6 +19,17 @@ const addToDb = (id) => {
   localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 };
 
+const getStoredCart = () => {
+  let shoppingCart = {};
+
+  //get the shopping cart from local storage
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  }
+  return shoppingCart;
+};
+
 const removeFromDb = (id) => {
   const storedCart = localStorage.getItem("shopping-cart");
   if (storedCart) {
@@ -34,5 +45,5 @@ const deleteShoppingCart = () => {
   localStorage.removeItem("shopping-cart");
 };
 
-export { addToDb, removeFromDb, deleteShoppingCart };
+export { addToDb, getStoredCart, removeFromDb, deleteShoppingCart };
 
